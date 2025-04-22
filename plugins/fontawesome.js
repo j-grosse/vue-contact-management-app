e8ce1d071dd4a3c538e10f604596f01f061e939d
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-04-22 19:52:28 +0200

    fix: fontawesome import

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -2,12 +2,55 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+/*
+import {
+  faHouse,
+  faCog,
+  faTh,
+  faList,
+  faUsers,
+  faPlus,
+  faChevronUp,
+  faChevronDown,
+  faPaperPlane,
+  faSearch,
+  faCheck,
+  faImage,
+  faTrash,
+  faUser,
+  faFloppyDisk,
+  faFileImport,
+  faMoon,
+  faSun,
+} from '@fortawesome/free-solid-svg-icons'
 
+// Add the icons you want
+library.add(
+  faHouse,
+  faCog,
+  faTh,
+  faList,
+  faUsers,
+  faPlus,
+  faChevronUp,
+  faChevronDown,
+  faPaperPlane,
+  faSearch,
+  faCheck,
+  faImage,
+  faTrash,
+  faUser,
+  faFloppyDisk,
+  faFileImport,
+  faMoon,
+  faSun
+)
+*/
 library.add(fas)
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
+  nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon, {})
 })

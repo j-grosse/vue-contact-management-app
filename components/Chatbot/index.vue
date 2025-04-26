6fa17000799bb1a23c9555be77588e66d6ded326
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-04-26 14:47:59 +0200

    feat: add pronoun detection and integrate data of events in Cologne

--- components/Chatbot/index.vue
+++ components/Chatbot/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div
-    class="bg-white dark:bg-yellow-900/20 rounded-lg shadow-md overflow-hidden my-2"
+    class="dark:bg-yellow-900/20 rounded-lg shadow-md overflow-hidden my-2 mb-4"
   >
     <!-- Chatbot window header -->
     <div
+      @click="isOpen = !isOpen"
       class="p-4 bg-primary text-white rounded-t-lg flex justify-between items-center"
     >
-      <h3 class="font-semibold">Kontaktassistent</h3>
+      <h3 class="font-semibold">Empfehlungsassistent</h3>
       <button @click="isOpen = !isOpen" class="text-white hover:text-gray-200">
         <FontAwesomeIcon
           :icon="isOpen ? 'fa-chevron-down' : 'fa-chevron-up'"
@@ -18,7 +19,7 @@
     <div v-if="isOpen" class="p-4">
       <div
         ref="chatContainerRef"
-        class="mb-4 h-64 overflow-y-auto bg-gray-50 dark:bg-gray-900 rounded-lg p-3"
+        class="mb-4 h-64 overflow-y-auto bg-gray-50 dark:bg-gray-900 border rounded-lg p-3"
       >
         <div v-for="(message, index) in chatMessages" :key="index" class="mb-3">
           <!-- User message -->
@@ -35,7 +36,11 @@
               <div v-if="message.isLoading" class="flex items-center">
                 <div class="dot-flashing"></div>
               </div>
-              <div v-else v-html="formatMessage(message.content)" class="markdown-body"></div>
+              <div
+                v-else
+                v-html="formatMessage(message.content)"
+                class="markdown-body"
+              ></div>
             </div>
           </div>
         </div>
@@ -47,7 +52,7 @@
           v-model="userInput"
           @keyup.enter="sendMessage"
           type="text"
-          placeholder="Frage nach Freizeitaktivitäten..."
+          placeholder="Frage nach Freunden, Freizeitaktivitäten etc. ..."
           class="flex-1 px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary dark:bg-gray-800 dark:text-white dark:border-gray-700"
         />
         <button
@@ -59,11 +64,11 @@
         </button>
       </div>
 
-      <div class="mt-3">
-        <p class="text-xs text-gray-500 dark:text-gray-400">
+      <!-- <div class="mt-3">
+        <p class="text-xs pl-2 text-gray-500 dark:text-gray-400">
           Tipps: Frage nach "Anna Schmidt" oder "Idee für 3 Freunde"
         </p>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -72,6 +77,8 @@
 import { ref, onMounted, nextTick } from 'vue';
 import { useFriendsStore } from '~/stores/friends';
 import { marked } from 'marked';
+import { useEvents } from '~/composables/useEvents';
+const { fetchEvents, events } = useEvents();
 
 const isOpen = ref(true);
 const isLoading = ref(false);
@@ -79,19 +86,25 @@ const userInput = ref('');
 const userInputRef = ref<HTMLInputElement | null>(null);
 const chatContainerRef = ref<HTMLElement | null>(null);
 
+type Event = {
+  id: string;
+  title: string;
+  date: string;
+  location: string;
+  description: string;
+  price: number | string;
+  link: string;
+};
+
+let eventList: string = '';
+
 type ChatMessage = {
   role: 'ai' | 'user';
   content: string;
   isLoading: boolean;
 };
 
-const chatMessages = ref<ChatMessage[]>([
-  {
-    role: 'ai',
-    content: 'Hallo!',
-    isLoading: false,
-  },
-]);
+const chatMessages = ref<ChatMessage[]>([]);
 
 type Friend = {
   id: number;
@@ -136,7 +149,7 @@ const buildFriendNotesContext = () => {
 const buildConversationContext = () => {
   const maxTokens = 1000; // API's token limit
   let context = '';
-  const recentMessages = chatMessages.value.slice(-10); // Start with the last 5 messages
+  const recentMessages = chatMessages.value.slice(-3); // Start with the last 5 messages
 
   for (const message of recentMessages) {
     const formattedMessage =
@@ -183,13 +196,13 @@ const sendMessage = async () => {
   isLoading.value = true;
 
   try {
-    // Prepare the final prompt
-    const conversationContext = buildConversationContext();
-    let finalPrompt = `${conversationContext}\n\nUser: ${userQuestion}\n\nAI:`;
-
-    // Check if the user question contains a friend's name
     const friendNotesContext = buildFriendNotesContext();
 
+    // Prepare the final prompt
+    const conversationContext = buildConversationContext();
+    let finalPrompt = `${conversationContext}\n\n ${friendNotesContext}\n\n User: ${userQuestion}\n\nAI: `;
+
+    // Check if the user question contains a friend's name
     const matchingFriend = friendStore.friends.find((friend) => {
       const nameParts = friend.name.toLowerCase().split(' '); // Split the friend's name into parts
       return nameParts.some((part) =>
@@ -203,17 +216,18 @@ const sendMessage = async () => {
 
     // if question contains a friend's name, use their notes
     if (matchingFriend) {
-      finalPrompt = `${conversationContext}\n\nAI: Du bist ein deutscher Recommendation-Chatbot. Der User fragt nach \n\n User: ${userQuestion}
-\n\n Wenn der User nur den Namen ${matchingFriend.name} als Promt gegeben hat, empfehle dem User ${matchingFriend.name} zu kontaktieren und gib einen Vorschlag für eine Nachricht basierend auf den folgenden
-Notizen: ${matchingFriend.notes}\n\n Ansonsten beantworte seine Frage zu ${matchingFriend.name}\n\n und den Notizen: ${matchingFriend.notes}
-Bitte antworte in mehreren Absätzen. Beginne deine Antwort mit "OK."`;
-      // if question contains a friend's name, use their notes
-    } else if (!matchingFriend && lastMentionedFriend) {
-      finalPrompt = `${conversationContext}\n\nAI: Der User hat zuvor nach ${lastMentionedFriend.name} gefragt. Beziehe das Pronomen in der Frage auf ${lastMentionedFriend.name} und ${lastMentionedFriend.notes}, wenn dieses zur Person passt und beantworte die Frage\n\nUser: ${userQuestion}`;
-    } else if (!matchingFriend) {
-      finalPrompt = `${conversationContext}\n\nAI: Du bist ein deutscher Recommendation-Chatbot. Der User hat folgende allgemeine Frage: \n\nUser: ${userQuestion}`;
-    }
+      finalPrompt += `Du bist ein deutscher Recommendation-Chatbot.
+      \n\n Mache Vorschläge für die Kontaktaufnahme mit ${matchingFriend.name} basierend auf den
+      Notizen \n\n ${matchingFriend.notes} \n\n Ansonsten beantworte seine Frage zu ${matchingFriend.name}\n\n und den Notizen: ${matchingFriend.notes}
+      Bitte antworte in mehreren Absätzen. Beginne deine Antwort mit "OK."`;
 
+      // if question contains a pronoun, use their notes
+    } else if (!matchingFriend && lastMentionedFriend) {
+      finalPrompt += `Der User hat zuvor nach ${lastMentionedFriend.name} gefragt. Beziehe das Pronomen in der Frage auf ${lastMentionedFriend.name} und ${lastMentionedFriend.notes}, wenn dieses zur Person passt und beantworte die Frage.`;
+    } else {
+      finalPrompt += `Wenn der User nach Events, Veranstaltungen oder Unternehmungen fragt, empfehle ihm zu einigen Freunden passende Events an:\n\n'<u><a href="{link}" target="_blank class="text-blue-500">{title}</a></u>'\n\n Eventliste: ${eventList}`
+    }
+    
     // Call the nuxt server's API with the final prompt
     const { text } = await $fetch('/api/chat', {
       method: 'POST',
@@ -238,14 +252,25 @@ Bitte antworte in mehreren Absätzen. Beginne deine Antwort mit "OK."`;
   }
 };
 
-// Make an initial API call on app start
+// Make initial API calls on app start
 onMounted(async () => {
+  // Fetch events on mount
+  await fetchEvents();
+  
+   // Convert fetched events JSON Array into clickable html tag, for use in prompt
+   eventList = events.value
+  .map(
+    (event: Event) =>
+      `${event.title}: ${event.link}`
+      // `<u><a href="${event.link}" target="_blank">${event.title}</a></u>`
+  )
+  .join(', ');
+  console.log(eventList);
+
   const friendDue = getNextFriendToContact();
   if (friendDue) {
-    const initialPrompt = `Du bist ein deutscher Recommendation-Chatbot. Empfehle dem User seinen Kontakt mit dem Namen ${friendDue.name} zu kontaktieren und gib einen Vorschlag für eine Nachricht aufgrund der folgenden Notizen zum letzten Gespräch mit dem Freund:
-    
-    ${friendDue.notes}
-    
+    const initialPrompt = `Du bist ein deutscher Recommendation-Chatbot. Schlage dem User eine Nachricht für die anstehende Kontaktaufnahme mit ${friendDue.name} vor.
+    Nutze die Notizen ${friendDue.notes}.
     Bitte antworte in mehreren Absätzen. Beginne deine Antwort mit: Stay in touch!`;
 
     try {

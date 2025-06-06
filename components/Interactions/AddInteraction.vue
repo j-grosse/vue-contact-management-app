05bb6b237d4adc7ec7099ba562e46ba64b39eec2
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-06-06 13:34:16 +0200

    feat: add interactions tab

---
+++ components/Interactions/AddInteraction.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="submitInteraction" class="add-interaction flex flex-col justify-between mb-6 gap-3">
+    <div class="flex w-full flex-col">
+        <input 
+          type="date" 
+          v-model="date" 
+          required 
+          class="date-input text-lg block text-gray-700 dark:text-gray-300 mb-1 mr-auto"
+          />
+        <textarea 
+          v-model="text" 
+          placeholder="Text..." 
+          required
+          class="text-input border rounded-lg p-3"
+          >
+      </textarea>
+    </div>
+    <button
+      type="submit"
+      class="submit-button px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 text-base px-4 py-2 ml-auto"
+      >
+      <FontAwesomeIcon icon="fa-floppy-disk" />
+    </button>
+  </form>
+</template>
+
+<script setup>
+const emit = defineEmits(['add']);
+const date = ref(new Date().toISOString().split('T')[0]);
+const text = ref('');
+
+const submitInteraction = () => {
+  emit('add', {
+    date: date.value,
+    text: text.value
+  });
+  text.value = '';
+};
+</script>
---
+++ components/Interactions/InteractionList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="interactions-list">
+    <!-- sort entries by date and reverse order -->
+    <div
+      v-for="interaction in [...interactions].sort((a, b) => new Date(a.date) - new Date(b.date)).reverse()"
+      :key="interaction.id"
+      class="interaction-item mb-3 border-b"
+    >
+    <div class="date text-lg block text-gray-700 dark:text-gray-300">
+        {{ formatDate(interaction.date) }}
+        <span class="text-lg text-gray-700 dark:text-gray-300 pl-1">
+            ({{ getDaysAgo(interaction.date) }})
+        </span>
+    </div>
+      <div class="text-md p-1 rounded-lg">{{ interaction.text }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  interactions: {
+    type: Array,
+    required: true
+  }
+});
+
+const formatDate = (date) => {
+  const d = new Date(date);
+  const day = d.getDate().toString().padStart(2, '0');
+  const month = (d.getMonth() + 1).toString().padStart(2, '0');
+  const year = d.getFullYear();
+  return `${day}.${month}.${year}`;
+};
+
+const getDaysAgo = (date) => {
+  const today = new Date();
+  const interactionDate = new Date(date);
+  
+  // Reset time part to compare only dates
+  today.setHours(0, 0, 0, 0);
+  interactionDate.setHours(0, 0, 0, 0);
+  
+  const diffTime = today - interactionDate;
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  
+  if (diffDays === 0) return 'Heute';
+  if (diffDays === 1) return 'Gestern';
+  if (diffDays < 21) return `Vor ${diffDays} Tagen`;
+  if (diffDays < 30) return `Vor ${Math.floor(diffDays / 7)} Wochen`;
+  if (diffDays < 365) return `Vor ${Math.floor(diffDays / 30)} Monaten`;
+  return `Vor ${Math.floor(diffDays / 365)} Jahren`;
+};
+</script>

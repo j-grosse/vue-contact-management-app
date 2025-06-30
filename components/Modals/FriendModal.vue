bf231813289b73d018633503ef3ed11dddf5ade3
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-06-30 17:20:55 +0200

    fix: move contact intervall field

--- components/Modals/FriendModal.vue
+++ components/Modals/FriendModal.vue
@@ -138,26 +138,12 @@
                 <textarea
                   v-model="form.notes"
                   placeholder="Notizen zu diesem Friend"
-                  rows="3"
+                  rows="2"
                   class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary dark:bg-gray-700 dark:text-white dark:border-gray-600 text-base"
                 ></textarea>
               </div>
 
               <div class="flex justify-between gap-3">
-                <!-- Contact Interval -->
-                <div>
-                  <label class="block text-gray-700 dark:text-gray-300 mb-1"
-                    >Intervall</label
-                  >
-                  <input
-                    type="number"
-                    v-model="form.contactInterval"
-                    min="1"
-                    required
-                    class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary dark:bg-gray-700 dark:text-white dark:border-gray-600 text-base"
-                  />
-                </div>
-
                 <!-- Last Contact Date -->
                 <div>
                   <label class="block text-gray-700 dark:text-gray-300 mb-1"
@@ -168,6 +154,19 @@
                     v-model="form.lastContactDate"
                     required
                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary dark:bg-gray-700 dark:text-white dark:border-gray-600 text-base"
+                  />
+                </div>
+                 <!-- Contact Interval -->
+                 <div>
+                  <label class="block text-gray-700 dark:text-gray-300 mb-1"
+                    >Intervall</label
+                  >
+                  <input
+                    type="number"
+                    v-model="form.contactInterval"
+                    min="1"
+                    required
+                    class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary dark:bg-gray-700 dark:text-white dark:border-gray-600 text-base text-right"
                   />
                 </div>
               </div>

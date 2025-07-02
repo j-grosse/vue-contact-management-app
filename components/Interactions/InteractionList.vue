2e2570e292ab1e1f86b520a8659bf05539ef9b47
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-07-02 17:06:05 +0200

    refactor: remove duplicate code

--- components/Interactions/InteractionList.vue
+++ components/Interactions/InteractionList.vue
@@ -35,25 +35,6 @@
             </button>
           </div>
         </div>
-        <div class="date text-lg block text-gray-700 dark:text-gray-300">
-          {{ getDaysAgo(interaction.date) }}
-          ({{ formatDate(interaction.date) }})
-        </div>
-        <div class="flex">
-          <div class="text-md text-gray-700 dark:text-gray-300 p-1 rounded-lg">{{ interaction.text }}</div>
-          
-          <div class="ml-auto">
-            <button
-              @click="editInteraction(interaction)"
-              class="px-2 py-2 rounded-lg text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
-              title="Edit interaction"
-            >
-              <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-              </svg>
-            </button>
-          </div>
-        </div>
       </div>
     </div>
 

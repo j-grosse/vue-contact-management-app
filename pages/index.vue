7287ea6f61b1930bc9d0cdf91944cc95dfade982
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-07-05 09:33:07 +0200

    fix: keep components mounted when tab changes

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
     <div class="tabs flex justify-center gap-2 bg-gray-300 dark:bg-gray-700 border-2 border-gray-300 dark:border-gray-600 rounded-lg p-1">
           <button
             :class="[
-              'w-1/2 py-2 rounded-lg font-medium transition-colors duration-150 focus:outline-none',
+              'w-1/2 py-2 rounded-lg font-medium transition-colors duration-150',
               activeTab === 'globalInteractions'
                 ? 'bg-white dark:bg-gray-800 text-primary-500 dark:text-gray-300 shadow-md'
                 : 'bg-transparent text-gray-600 dark:text-gray-300'
@@ -29,7 +29,7 @@
           </button>
           <button
             :class="[
-              'w-1/2 py-2 rounded-lg font-medium transition-colors duration-150 focus:outline-none',
+              'w-1/2 py-2 rounded-lg font-medium transition-colors duration-150',
               activeTab === 'chatbot'
                 ? 'bg-white dark:bg-gray-800 text-primary-500 dark:text-gray-300 shadow-md'
                 : 'bg-transparent text-gray-600 dark:text-gray-300'
@@ -41,16 +41,16 @@
           </button>
         </div>
 
-           <!-- Interactions Tab active  -->
-           <div v-if="activeTab === 'globalInteractions'">
+        <!-- Interactions Tab active  -->
+        <div v-show="activeTab === 'globalInteractions'">
           <GlobalInteractions @edit="editFriend"
             />  
         </div>
 
         <!-- Chatbot Tab active  -->
-        <div v-if="activeTab === 'chatbot'">
-          <Chatbot />
-        </div>
+          <div v-show="activeTab === 'chatbot'">
+            <Chatbot />
+          </div>
 
     <TableView
       v-if="appStore.viewMode === 'table'"
